ade8b76ec6cc5b48dfa0b407310cd0182296ecc2
bingoring <ritch1530@gmail.com>
2024-04-29 14:31:20 +0900

    fix: sse messageConsumer init

--- product/gateway/src/router/sse/sse.service.ts
+++ product/gateway/src/router/sse/sse.service.ts
@@ -1,19 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { Observable, fromEvent } from 'rxjs';
 import { MessageConsumer } from '@root/message-queue/index';
 import { SseResponseDto } from './dto/sse.dto';
 
 @Injectable()
-export class SseService {
+export class SseService implements OnModuleInit {
     constructor(private readonly eventEmitter: EventEmitter2, private readonly messageConsumer: MessageConsumer) {}
-    public async sse(tenantId: string) {
-        const result = fromEvent(this.eventEmitter, tenantId) as Observable<SseResponseDto>;
+    public onModuleInit() {
         this.messageConsumer.subscribe((msg) => {
             const messageData = JSON.parse(msg);
             const messageEvent = { value: messageData };
             this.eventEmitter.emit((messageData as any).tenantId, messageEvent);
         });
+    }
+
+    public async sse(tenantId: string) {
+        const result = fromEvent(this.eventEmitter, tenantId) as Observable<SseResponseDto>;
         return result;
     }
 }
